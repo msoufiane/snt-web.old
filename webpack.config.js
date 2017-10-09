7d12d38f73af542e2f978e6400548efd62e138fc
Soufiane Mghanen <soufiaane.m@avito.ma>
2017-10-09 21:38:36 +0100

    Second running version with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
-const path = require('path');
+const {resolve} = require('path');
 
-module.exports = {
-    entry: './src/index.js',
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+const config = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        resolve(__dirname, 'src/index.js'),
+    ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: resolve(__dirname, 'dist'),
+        publicPath: '',
+    },
+    // context: resolve(__dirname, 'src'),
+    devServer: {
+        hot: true,
+        contentBase: resolve(__dirname, 'build'),
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -14,7 +32,30 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=images/[name].[ext]' },
+            { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?&name=fonts/[name].[ext]' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
+            { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=images/[name].[ext]' },
         ]
-    }
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            test: /\.js$/,
+            options: {
+                eslint: {
+                    configFile: resolve(__dirname, '.eslintrc'),
+                    cache: false,
+                }
+            },
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
+        // new CopyWebpackPlugin([{ from: 'vendors', to: 'vendors' }]),
+        new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 };
+
+module.exports = config;
