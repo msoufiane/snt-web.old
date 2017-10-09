d04568292d0577842ca1817dd5d0daba4699523b
Soufiane Mghanen <soufiaane.m@avito.ma>
2017-10-09 21:08:32 +0100

    First running version with webpack

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import routes from './routes';
 import $ from 'jquery';
 
 // Import css
-import 'admin-lte/dist/css/skins/skin-purple.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'admin-lte/dist/css/AdminLTE.min.css';
-import 'ionicons/css/ionicons.min.css';
+// import 'admin-lte/dist/css/skins/skin-purple.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'admin-lte/dist/css/AdminLTE.min.css';
+// import 'ionicons/css/ionicons.min.css';
 
 window.jQuery = window.$ = $;
 require('bootstrap');
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,20 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const config = {
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src/index.js'
-    ],
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: [
-                'react-hot-loader',
-                'babel-loader'
-            ]
-        },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
+const path = require('path');
 
-                })
-            },
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+                use: {
+                    loader: 'babel-loader'
+                }
             }
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.css'],
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
-        })
-    ],
-    devServer: {
-        contentBase: './dist',
-        historyApiFallback: true
+        ]
     }
-
-}
-if (process.env.NODE_ENV === 'production') {
-    config.devtool = "cheap-module-source-map";
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-        new ExtractTextPlugin({
-            filename: 'bundle.css',
-            disable: false,
-            allChunks: true
-        }),
-        new webpack.optimize.AggressiveMergingPlugin({
-            minSizeReduce: 1,
-            moveToParents: true
-
-        })
-    )
-
-} else {
-    config.devtool = "cheap-module-eval-source-map";
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-        new ExtractTextPlugin({ disable: true })
-    )
-}
-
-module.exports = config;
+};
