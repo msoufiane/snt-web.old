7177536df8b31d2b3312f2de0a35e71faffc7aea
Soufiane Mghanen <soufiaane.m@avito.ma>
2017-10-23 18:00:14 +0100

    Fixing coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  "moduleNameMapper": {
+  moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
     "\\.(css|less)$": "<rootDir>/tools/assetsTransformer.js"
   },
-  "setupFiles": [
+  setupFiles: [
     "raf/polyfill",
     "./tools/enzymeTestAdapterSetup.js"
-  ]
+  ],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/registerServiceWorker.js",
+    "!src/webpack-public-path.js"
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules',
+    '/coverage/',
+  ],
 };
