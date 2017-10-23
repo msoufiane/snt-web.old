fbdf7b47413a79e2e0a457558c598f2ca3e9e402
Soufiane Mghanen <soufiaane.m@avito.ma>
2017-10-23 20:50:20 +0100

    improving experss server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // in server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Since the root/src dir contains our index.html
 app.use(express.static(__dirname + '/dist/'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
 
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
