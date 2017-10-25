313a310f38edf86fe3e261777d0959f07ec500f4
Soufiane Mghanen <mgh.soufiane@gmail.com>
2017-10-25 22:03:52 +0000

    Merged in dev (pull request #9)
    Dev

---
+++ src/App.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-named-as-default */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Route, Switch} from 'react-router-dom';
+
+import NavbarComponent from './components/layout/Navbar';
+import SidebarComponent from './components/layout/Sidebar';
+import FooterComponent from './components/layout/Footer';
+import ContentWrapperComponent from './components/layout/ContentWrapper';
+import {AuthenticatedRoute} from './Routes';
+
+import Login from './screens/login';
+import HomePage from './screens/HomePage';
+
+class App extends Component {
+  render() {
+    const {isLogged} = this.props;
+    return (
+      <div className="main">
+        <NavbarComponent isLogged={isLogged}/>
+        <ContentWrapperComponent isLogged={isLogged}>
+          <aside className={isLogged ? "main-sidebar" : ""}>
+            <section className="sidebar">
+              {isLogged && <SidebarComponent/>}
+            </section>
+            <section className="content">
+              <Switch>
+                <Route path="/login" component={Login}/>
+                <AuthenticatedRoute exact path="/" component={HomePage}/>
+              </Switch>
+            </section>
+          </aside>
+        </ContentWrapperComponent>
+        {isLogged && <FooterComponent/>}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLogged: !!state.authUser.token
+  };
+};
+
+export default connect(mapStateToProps, null)(App);
---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+export const AuthenticatedRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render={props => (
+    props.isLogged ? (<Component {...props}/>) :
+      (<Redirect to={{pathname: '/login', state: {from: props.location}}}/>)
+  )}/>
+);
+
