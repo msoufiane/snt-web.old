bb459300d965f6355ff6a86e2766f836e445b9c7
Soufiane Mghanen <mgh.soufiane@gmail.com>
2017-11-10 19:46:53 +0000

    Fixinf failing CodeClimate

--- src/components/authentication/loginForm/index.js
+++ src/components/authentication/loginForm/index.js
@@ -2,7 +2,7 @@
  * Created by soufiaane on 7/14/17.
  */
 
-// import React from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 import {reduxForm, Field, SubmissionError} from 'redux-form';
@@ -70,7 +70,7 @@ class LoginForm extends React.Component {
             </button>
           </div>
         </div>
-        <Field name='serverError' component={this.serverError}/>
+        <Field name="serverError" component={this.serverError}/>
       </form>
     );
   }
